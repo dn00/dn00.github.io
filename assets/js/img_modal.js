0e75288e04976823f0e8b60b40826e43d0cf2d75
dn00 <dtnguy90@gmail.com>
2018-08-29 14:09:23 -0700

    modal test

--- assets/js/img_modal.js
+++ assets/js/img_modal.js
@@ -1,8 +1,20 @@
-$(window).load(function(){
-    $('img').on('click',function()
-                {
-                    var sr=$(this).attr('src'); 
-                    $('#mimg').attr('src',sr);
-                    $('#myModal').modal('show');
-                });
-    });
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
